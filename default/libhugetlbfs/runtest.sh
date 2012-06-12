f76b8121a1a5691d38d525f3b9652506ed1e8090
Josh Boyer <jwboyer@redhat.com>
2012-06-12 15:46:54 -0400

    Add libhugetlbfs tests

---
+++ default/libhugetlbfs/runtest.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+source ../../utils/root-check.sh
+
+check_root
+is_root=$?
+if [ "$is_root" -ne "0" ]; then
+	exit 3
+fi
+
+# Build
+cd libhugetlbfs
+make BUILDTYPE=NATIVEONLY > /dev/null 2>/dev/null
+if [ "$?" -ne "0" ]; then
+	echo "Could not build libhugetlbfs tests"
+	exit -1
+fi
+
+# Setup: Need at least 32 free hugepages for the shm test to run
+obj/hugeadm --pool-pages-min 2MB:32
+
+# Run
+make BUILDTYPE=NATIVEONLY func > ../libhugetlbfs_results.tmp
+if [ "$?" -ne "0" ]; then
+	echo "Could not run tests"
+	exit -1
+fi
+
+# look at stuff
+# TODO
+# Remove the readahead test.  It seems it's known to always fail.
+sed -e 's/readahead_reserve.sh .*//' ../libhugetlbfs_results.tmp > ../libhugetlbfs_results.tmp2
+
+# Filter out the morecore tests too.  Question sent into the list about those
+sed -e 's/HUGETLB_MORECORE=.*//' ../libhugetlbfs_results.tmp2 > ../libhugetlbfs_results.txt
+
+grep -v "FAIL:" ../libhugetlbfs_results.txt | grep "FAIL"
+if [ "$?" == "0" ]; then
+	echo "Test failures"
+	exit -1
+fi
+
+# Cleanup
+obj/hugeadm --pool-pages-min 2MB:0
+obj/hugeadm --pool-pages-max 2MB:0
+
+make clean > /dev/null 2> /dev/null
+cd ..
+rm -rf libhugeltbfs_results.*
