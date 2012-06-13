cef10d027b9cb91165e8c1ee7cccffdc6b38baf0
Josh Boyer <jwboyer@redhat.com>
2012-06-13 14:11:50 -0400

    Add a path to ignore-files.sh invocation in sysfs-perms

--- default/sysfs-perms/runtest.sh
+++ default/sysfs-perms/runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COUNT=$(find /sys -type f -perm 666 | ignore-files.sh | wc -l)
+COUNT=$(find /sys -type f -perm 666 | ./ignore-files.sh | wc -l)
 
 if [ "$COUNT" != "0" ]; then
 	echo Found world-writable files in sysfs.
