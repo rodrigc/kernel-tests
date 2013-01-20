1291c93aeacfa3c8e3253c12c1248672af59e02b
Justin M. Forbes <jforbes@redhat.com>
2013-01-20 11:22:24 -0600

    Add module check utility

--- utils/mod-check.sh
+++ utils/mod-check.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# This function will check to see if a module is loaded. It is usefule
+# for tests which require specific modules or hardware.  If a module
+# is not loaded, the result should typically be a skip, not a failure.
+# Example usage in your runtest.sh:
+#
+# source ../../utils/mod-check.sh
+# module=foobar
+# check_mod
+# has_mod=$?
+# 
+# if [ "$has_mod" -ne "0" ]; then
+#         exit 3
+# fi
+
+
 check_mod() {
 	if [ $(lsmod | grep -wc $module ) -eq 0 ]
 	then
