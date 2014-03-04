8118a1e821c8613114f68ff3777857e380c8a353
Justin M. Forbes <jforbes@redhat.com>
2014-03-04 13:47:38 -0600

    skip stack-randomness on 32bit

--- default/stack-randomness/runtest.sh
+++ default/stack-randomness/runtest.sh
@@ -2,7 +2,14 @@
 #
 # Licensed under the terms of the GNU GPL License version 2
 
-COUNT=$(for i in `seq 1 1000` ; do grep stack /proc/self/maps; done  | sort -u | uniq -c | wc -l)
+ARCH=$(uname -m)
+
+if [ "$ARCH" == "x86_64" ]; then
+  COUNT=$(for i in `seq 1 1000` ; do grep stack /proc/self/maps; done  | sort -u | uniq -c | wc -l)
+else
+  exit 3
+fi
+
 
 if [ $COUNT -lt 950 ]; then
   echo "Stack randomness changes < 95% of the time ($COUNT)"
